b1aece61dac0b26bfbfd4a8d1896caf3370697d8
jesus <jesuscumpli@gmail.com>
2022-05-04 09:00:13 +0200

    Automatic Anonymization

--- utils/examples/exampleAnonymization.py
+++ utils/examples/exampleAnonymization.py
@@ -1,8 +1,10 @@
+import time
+
 import pandas as pd
 import numpy as np
 
 from utils.Anonymization import Anonymization
-from utils.techniques.generalization import generalization_numerical_mask, generalization_categorical_semantic, \
+from utils.techniques.generalization import generalization_categorical_semantic, \
     generalization_numerical_interval
 from utils.techniques.pseudonymization import *
 from scipy import stats
@@ -10,49 +12,62 @@ from scipy import stats
 dfOrigen = pd.read_csv("../datasets/football_salaries.csv")
 dfOrigen = dfOrigen.dropna()
 dfOrigen = dfOrigen.sort_values(by='player', ascending=True)
+dfOrigen = dfOrigen.head(100)
 
 identifiers = ["player"]
 quasi_identifiers = ["position", "team", "age"]
-sensible = ["total_value", "avg_year", "total_guaranteed", "fully_guaranteed"]
-non_sensible = ["free_agency"]
+sensible = ["total_value", "avg_year"]
 
-# Pseudonimization identifiers
-dfFinal, dfMapping = pseudonymization_counter(dfOrigen, identifiers[0])
+# # Pseudonimization identifiers
+# dfFinal, dfMapping = pseudonymization_counter(dfOrigen, identifiers[0])
+#
+# # Generalization quasi_identifiers
+# dfFinal = generalization_numerical_mask(dfFinal, "age", 1)
 
-# Generalization quasi_identifiers
-dfFinal = generalization_numerical_mask(dfFinal, "age", 1)
+# dfFinal = generalization_categorical_semantic(dfFinal, "position", semantic_herarchy)
+#
+# # Generalization sensible
+# intervals = [-np.inf, 0, 1000, 5000, 10000, 50000, 100000, 200000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000,
+#              200000000, 500000000, np.inf]
+# dfFinal = generalization_numerical_interval(dfFinal, "total_value", intervals)
+# dfFinal = generalization_numerical_interval(dfFinal, "avg_year", intervals)
+# dfFinal = generalization_numerical_interval(dfFinal, "total_guaranteed", intervals)
+# dfFinal = generalization_numerical_interval(dfFinal, "fully_guaranteed", intervals)
+
 semantic_herarchy = {
     "defense": ["cornerback", "defensive-back", "safety", "linebacker", "interior-defensive-line"],
     "medium": ["center", "right-guard", "3-4-defensive-end"],
     "attack": ["offensive-line", "right-tackle", "punter", "quarterback"]
 }
-dfFinal = generalization_categorical_semantic(dfFinal, "position", semantic_herarchy)
+semantics = {}
+semantics["position"] = semantic_herarchy
 
-# Generalization sensible
-intervals = [-np.inf, 0, 1000, 5000, 10000, 50000, 100000, 200000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000,
-             200000000, 500000000, np.inf]
-dfFinal = generalization_numerical_interval(dfFinal, "total_value", intervals)
-dfFinal = generalization_numerical_interval(dfFinal, "avg_year", intervals)
-dfFinal = generalization_numerical_interval(dfFinal, "total_guaranteed", intervals)
-dfFinal = generalization_numerical_interval(dfFinal, "fully_guaranteed", intervals)
+model = Anonymization(dfOrigen, identifiers, quasi_identifiers, sensible, semantics)
 
-data1 = dfFinal.value_counts(quasi_identifiers)
+time1 = time.time()
+dfFinal, best_utility = model.achieve_klt_random(k=2, l=2, t=0.2, stop_utility=0.7, MAX_ITERS=100)
+time2 = time.time()
+time_interval = time2 - time1
+print("Tiempo: " + str(time_interval) + "; utility: " + str(best_utility))
 
-model = Anonymization(dfFinal, identifiers, quasi_identifiers, non_sensible, sensible)
+model.reset_dataframe_final()
 
-model.achieve_k_anonymity(2)
-if model.check_k_anonymity_better(2):
+time1 = time.time()
+dfFinal, best_utility = model.achieve_klt_backtracking(k=2, l=2, t=0.2, stop_utility=0.7, MAX_ITERS=100)
+time2 = time.time()
+time_interval = time2 - time1
+print("Tiempo: " + str(time_interval) + "; utility: " + str(best_utility))
+
+if model.check_k_anonymity(2):
     print("K ANONYMITY ACCEPTED!")
 else:
     print("NO K ANONYMITY!!!!!")
 
-model.achieve_l_diversity(2)
 if model.check_l_diversity(2):
     print("L DIVERSITY ACCEPTED!")
 else:
     print("NO L DIVERSITY!!!!!")
 
-model.achieve_t_closeness(0.05)
 if model.check_t_closeness(0.05):
     print("T CLOSENESS ACCEPTED!")
 else:
--- utils/techniques/generalization.py
+++ utils/techniques/generalization.py
@@ -12,18 +12,22 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 
-def generalization_numerical_interval(dataframe, col, intervals):
+def generalization_numerical_interval(dataframe, col, step):
+    quantiles = np.arange(0, 1, step)
+    intervals = dataframe[col].quantile(quantiles)
+    intervals = intervals.drop_duplicates()
+    intervals = intervals.tolist()
     dfFinal = dataframe.copy(deep=True)
     dfFinal[col] = pd.cut(dfFinal[col], bins=intervals)
     return dfFinal
 
-def generalization_numerical_mask(dataframe, col, num_mask):
+def generalization_mask(dataframe, col, num_mask):
     dfFinal = dataframe.copy(deep=True)
     mask = "*" * num_mask
     for index, row in dfFinal.iterrows():
         number = str(row[col])
         masked = number[:-num_mask] + mask
-        dfFinal[col][index] = masked
+        dfFinal.at[index, col] = masked
     return dfFinal
 
 def generalization_categorical_semantic(dataframe, col, hierarchy):
@@ -35,5 +39,5 @@ def generalization_categorical_semantic(dataframe, col, hierarchy):
             if value in values:
                 res = key
                 break
-        dfFinal[col][index] = res
+        dfFinal.at[index, col] = res
     return dfFinal
